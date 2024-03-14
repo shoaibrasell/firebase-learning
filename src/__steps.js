939794cc85ca53b58d1f5ef7537c683beae5d7b6
rasel <shoaibullahrasel@gmail.com>
2024-03-14 16:35:49 +0600

    added the firebase setup

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,6 @@
 /**
+ *   ---This are the step to install the firebase setup ----
+ * 
  * 1.visit console.firebase.google.com
  * 2.create project (skip google analytics)
  * 3.Register app (create config)
@@ -6,5 +8,11 @@
  * 5.add config file to your project
  * 6.Danger:: Do not publish or make firebase config to public by pushing to github
  * 7.Visit : Go to Docs > Build ? Authentication > Web > Get Started 
- * 8. Export app from the firebase.config.js file
+ * 8.Export app from the firebase.config.js file export default app
+ * 9.Login.jsx: import getAuth from Firebase/auth  
+ * 10.create const auth = getAuth
+ * 11.import googleAuthProvider and create a new provider
+ * 12.use signInWithPopUp and pass auth and provider
+ * 13.Activate sign-in methods (google facebook github etc)
+ * 
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { getAuth } from 'firebase/auth'
-import app from '../../firebase/firebase.init';
+import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
-  const auth = getAuth(app)
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
   return (
     <div>
-      <button>Google Login</button>
+      <button onClick={handleGoogleSignIn}>Google Login</button>
     </div>
   );
 };
